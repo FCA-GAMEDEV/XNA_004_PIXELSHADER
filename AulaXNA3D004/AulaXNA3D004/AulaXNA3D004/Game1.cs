153764bfaacd44e0224d86318bcbed374d61e00c
FCA-GAMEDEV <85238682+FCA-GAMEDEV@users.noreply.github.com>
2021-06-02 16:14:41 -0300

    Add files via upload

---
+++ AulaXNA3D004/AulaXNA3D004/AulaXNA3D004/Game1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace AulaXNA3D004
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        _Screen screen;
+        _Camera camera;
+        _Quad quad;
+
+        MouseState mouse;
+        float radius = 0.2f;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+        }
+
+        protected override void Initialize()
+        {
+            this.screen = _Screen.GetInstance();
+            this.screen.SetWidth(graphics.PreferredBackBufferWidth);
+            this.screen.SetHeight(graphics.PreferredBackBufferHeight);
+
+            this.camera = new _Camera();
+
+            this.quad = new _Quad(GraphicsDevice, this, @"Textures\logoJD", @"Effects\Effect1");
+
+            this.mouse = new MouseState();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+            this.quad.Dispose();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            this.quad.Update(gameTime);
+
+            this.mouse = Mouse.GetState();
+
+            float aux = 0.2f + Math.Abs(this.mouse.ScrollWheelValue * gameTime.ElapsedGameTime.Milliseconds * 0.00001f);
+            this.radius =  aux < 0.2f ? 0.2f : aux;
+
+            Window.Title = "AulaXNA3D004 - Radius: " + this.radius.ToString();
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            this.quad.Draw(this.camera, new Vector2(this.mouse.X / (float)this.screen.GetWidth(),
+                                                    this.mouse.Y / (float)this.screen.GetHeight()),
+                                                    this.radius);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ AulaXNA3D004/AulaXNA3D004/AulaXNA3D004/_Quad.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace AulaXNA3D004
+{
+    public class _Quad
+    {
+        Game game;
+        GraphicsDevice device;
+        Matrix world;
+        VertexPositionTexture[] verts;
+        VertexBuffer buffer;
+        Effect effect;
+        Texture2D texture;        
+
+        public _Quad(GraphicsDevice device, Game game, string textureName, string effectName)
+        {
+            this.game = game;
+            this.device = device;
+            this.world = Matrix.Identity;
+
+            this.verts = new VertexPositionTexture[]
+            {
+                new VertexPositionTexture(new Vector3(-1, 1,0),Vector2.Zero),  //v0
+                new VertexPositionTexture(new Vector3( 1, 1,0),Vector2.UnitX), //v1
+                new VertexPositionTexture(new Vector3(-1,-1,0),Vector2.UnitY), //v2
+
+                new VertexPositionTexture(new Vector3( 1, 1,0),Vector2.UnitX), //v3
+                new VertexPositionTexture(new Vector3( 1,-1,0),Vector2.One),   //v4
+                new VertexPositionTexture(new Vector3(-1,-1,0),Vector2.UnitY), //v5
+            };
+
+            this.buffer = new VertexBuffer(this.device,
+                                           typeof(VertexPositionTexture),
+                                           this.verts.Length,
+                                           BufferUsage.None);
+            this.buffer.SetData<VertexPositionTexture>(this.verts);
+
+            this.effect = this.game.Content.Load<Effect>(effectName);
+
+            this.texture = this.game.Content.Load<Texture2D>(textureName);
+
+        }
+
+        public void Dispose()
+        {
+            this.effect.Dispose();
+            this.texture.Dispose();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public virtual void Draw(_Camera camera, Vector2 mouse, float radius)
+        {
+            this.device.SetVertexBuffer(this.buffer);
+
+            this.effect.CurrentTechnique = this.effect.Techniques["Technique1"];
+            this.effect.Parameters["World"].SetValue(this.world);
+            this.effect.Parameters["View"].SetValue(camera.GetView());
+            this.effect.Parameters["Projection"].SetValue(camera.GetProjection());
+            this.effect.Parameters["colorTexture"].SetValue(this.texture);
+            this.effect.Parameters["mouse"].SetValue(mouse);
+            this.effect.Parameters["radius"].SetValue(radius);
+
+            foreach (EffectPass pass in this.effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                this.device.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleList,
+                                                                    this.verts, 0, 2);
+            }
+        }
+    }
+}
